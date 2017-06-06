d913ad63a26c8ab430ced5c8d7a3942758067401
BurgundyDev <bartekkulczycki@gmail.com>
2017-06-06 21:22:31 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+ int x = 0;
+
+ cout << "Kalkulator dzielnik\242w danej liczby\nTw\242rca - Bartosz Kulczycki\nKod \253r\242d\210\242wy udost\251pniony na github.com/BurgundyDev/cppprojects\nGNU General Public License v3.0\n";
+
+ cout << "\nWprowad\253 liczb\251: ";
+
+ cin >>x;
+
+ cout << "Dzielniki danej liczby: ";
+
+ for(int i=1;i<=x;i++)
+ {
+ if (x % i ==0) cout << i<<" ";
+ }
+ getch();
+}
