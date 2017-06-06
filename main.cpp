cd078fd09041f5cc1659dd97eb3ad31a24672dec
BurgundyDev <bartekkulczycki@gmail.com>
2017-06-06 21:29:28 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
 {
  int x = 0;
 
- cout << "Kalkulator dzielnik\242w danej liczby\nTw\242rca - Bartosz Kulczycki\nKod \253r\242d\210\242wy udost\251pniony na github.com/BurgundyDev/cppprojects\nGNU General Public License v3.0\n";
+ cout << "Kalkulator dzielnik\242w danej liczby\nTw\242rca - Bartosz Kulczycki\nKod \253r\242d\210\242wy udost\251pniony na github.com/BurgundyDev/Dzielniki\nGNU General Public License v3.0\n";
 
  cout << "\nWprowad\253 liczb\251: ";
 
